61a6f3535f450f2b5cbab748c9852ae39ac5edee
Katherine Bacon <kabacon@live.unc.edu>
2023-03-30 13:39:28 -0400

    Added census data, joined with airport data

--- HW4.R
+++ HW4.R
@@ -14,3 +14,30 @@ airports = airports %>% filter(passengers >= 10000) %>%
 
 #Found that the most popular destination from Raleigh is ATL, with 540190 passengers. 
 
+#Now starting question 2, retrieving data from tidycensus
+
+acs_vars = load_variables(2021, "acs5")
+write_csv(acs_vars, "acsvars.csv")
+
+
+
+demographics = get_acs(
+  geography="cbsa",  
+  variables=c(
+    "total_pop"="B01003_001"
+  ),
+  year=2021,
+  survey="acs5",
+  output="wide",
+)
+#making characters so I can join them
+airports$origin_cbsa = as.character(airports$origin_cbsa)
+airports$dest_cbsa = as.character(airports$dest_cbsa)
+
+#creating copies for join and made geo read as a character above. 
+origin_pop = demographics %>% rename("origin_cbsa" = "GEOID") 
+dest_pop = demographics %>% rename("dest_cbsa" = "GEOID") 
+
+airport_demographics = left_join(airports, origin_pop, by="origin_cbsa")
+airport_demographics = left_join(airports, dest_pop, by="dest_cbsa")
+

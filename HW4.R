4979900c45170253284650ac97ae2b3448df6e80
Katherine Bacon <kabacon@live.unc.edu>
2023-04-02 13:58:23 -0400

    Finished question 3

--- HW4.R
+++ HW4.R
@@ -24,7 +24,8 @@ write_csv(acs_vars, "acsvars.csv")
 demographics = get_acs(
   geography="cbsa",  
   variables=c(
-    "total_pop"="B01003_001"
+    "total_pop"="B01003_001",
+    "income" = "B17001_001"
   ),
   year=2021,
   survey="acs5",
@@ -35,9 +36,71 @@ airports$origin_cbsa = as.character(airports$origin_cbsa)
 airports$dest_cbsa = as.character(airports$dest_cbsa)
 
 #creating copies for join and made geo read as a character above. 
-origin_pop = demographics %>% rename("origin_cbsa" = "GEOID") 
-dest_pop = demographics %>% rename("dest_cbsa" = "GEOID") 
+origin_pop = demographics %>% rename("origin_cbsa" = "GEOID", "origin_pop" = "total_popE", "origin_income" = "incomeE") 
+dest_pop = demographics %>% rename("dest_cbsa" = "GEOID", "dest_pop" = "total_popE", "dest_income" = "incomeE") 
 
-airport_demographics = left_join(airports, origin_pop, by="origin_cbsa")
-airport_demographics = left_join(airports, dest_pop, by="dest_cbsa")
+airport_demographics = left_join(airports, origin_pop, by="origin_cbsa") %>%
+left_join(dest_pop, by="dest_cbsa")
+
+
+#Creating a CBSA to CBSA dataset
+
+CBSAs = airport_demographics %>% group_by(origin_cbsa, dest_cbsa) %>% summarize(total_passengers = sum(passengers))
+
+#defining a metro as atleast 50,000 people- all of these airports are in metro areas
+
+#creating models for each one of my variabels to make it easier to plot
+origin_model = lm(passengers~origin_pop, airport_demographics)
+summary(origin_model)
+
+dest_model = lm(passengers~dest_pop, airport_demographics)
+summary(dest_model)
+
+dist_model = lm(passengers~distancemiles, airport_demographics)
+summary(dist_model)
+
+#origin plot
+ggplot(airport_demographics, aes(x=origin_pop, y=passengers)) +
+  geom_point(size=0.5) +
+  geom_abline(slope = coef(origin_model)[[2]], intercept = coef(origin_model)[[1]])
+#positive association
+#dest plot
+ggplot(airport_demographics, aes(x=dest_pop, y=passengers)) +
+  geom_point(size=0.5)+
+  geom_abline(slope = coef(dest_model)[[2]], intercept = coef(dest_model)[[1]])
+#positive association
+#distance plot
+ggplot(airport_demographics, aes(x=distancemiles, y=passengers)) +
+  geom_point(size=0.5)+
+  geom_abline(slope = coef(dist_model)[[2]], intercept = coef(dist_model)[[1]])
+#negative association
+
+
+#extra credit- now will look at the income/poverty status of each origin and destination
+
+origin_model_income = lm(passengers~origin_income, airport_demographics)
+summary(origin_model_income)
+
+dest_model_income = lm(passengers~dest_income, airport_demographics)
+summary(dest_model_income)
+
+
+#origin plot
+ggplot(airport_demographics, aes(x=origin_income, y=passengers)) +
+  geom_point(size=0.5) +
+  geom_abline(slope = coef(origin_model_income)[[2]], intercept = coef(origin_model_income)[[1]])
+#positive association between the origin income and number of passengers
+#dest plot
+ggplot(airport_demographics, aes(x=dest_income, y=passengers)) +
+  geom_point(size=0.5)+
+  geom_abline(slope = coef(dest_model_income)[[2]], intercept = coef(dest_model_income)[[1]])
+#positive association between the destination income and number of passengers
+
+#this seems expected
+
+
+#Question 3
+
+
+ 
 
